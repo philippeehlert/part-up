06d80bb40c7119ccbc9b9bec9163501eb2f5ba57
Nick Koster <nick.koster@lifely.nl>
2017-04-13 15:52:23 +0200

    feat(file-upload): added remove functionality for files service

--- app/packages/partup-server/services/files_service.js
+++ app/packages/partup-server/services/files_service.js
@@ -36,5 +36,22 @@ Partup.server.services.files = {
         Files.insert(file);
 
         return file;
+    },
+
+    /**
+     * Remove a file
+     *
+     * @param {String} id
+     * @param {String} filename
+     */
+    remove: function(id, filename) {
+        var s3 = new AWS.S3({params: {Bucket: process.env.AWS_BUCKET_NAME}});
+        var filekey = id + '-' + filename;
+
+        // Remove from S3
+        s3.deleteObjectSync({Key: 'files/' + filekey});
+
+        // Remove from DB
+        Files.remove(id);
     }
 };
