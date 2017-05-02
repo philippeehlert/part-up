f5abc87cabc6e64de1c6150c989ce6b260a98dcf
Wouter Danes <wouter.danes@gmail.com>
2017-05-02 12:05:17 +0200

    fix(marketo): fixed script loading

--- app/packages/partup-client-base/marketto.js
+++ app/packages/partup-client-base/marketto.js
@@ -1,34 +1,35 @@
 import { Session } from 'meteor/session';
 import { Tracker } from 'meteor/tracker';
-import { _ } from 'meteor/underscore';
 import { $ } from 'meteor/jquery';
 
-const marketto = {
-    didInit: false,
+const marketo = {
+    loaded: false,
     loadScript() {
         function initMunchkin() {
-            if (didInit === false) {
-                didInit = true
-                Munchkin.init('449-VWG-450')
+            if (!loaded) {
+                loaded = true;
+                Munchkin.init('449-VWG-450');
             }
         }
-        var s = document.createElement('script')
-        s.type = 'text/javascript'
-        s.async = true
-        s.src = '//munchkin.marketo.net/munchkin.js'
-        s.onreadystatechange = function () {
-            if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                initMunchkin()
-            }
-        };
-        s.onload = initMunchkin
-        document.getElementsByTagName('body')[0].appendChild(s)
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//munchkin.marketo.net/munchkin.js';
+        if (s.readyState) {
+            s.onreadystatechange = () => {
+                if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                    initMunchkin();
+                }
+            };
+        }
+        s.onload = initMunchkin;
+        document.getElementsByTagName('body')[0].appendChild(s);
     }
-}
+};
 
 Tracker.autorun((computation) => {
-    if ((Cookies.get('cb-enabled') === 'enabled' || Session.get('cookiesEnabled') === 'enabled') && !marketto.didInit) {
-        $(window).on('load', marketto.loadScript)
-        computation.stop()
+    if ((Cookies.get('cb-enabled') === 'enabled' || Session.get('cookiesEnabled') === 'enabled') && !marketo.loaded) {
+        $(window).on('load', marketo.loadScript);
+        computation.stop();
     }
-})
+});
