7b26160db201cadd0ddc50ff860c4ec50e71fde2
Wouter (W.) Danes <wouter.danes@ing.nl>
2017-05-30 23:32:52 +0200

    fix(release): Moved imports to requires for gulp tasks

--- gulp/tasks/bump.js
+++ gulp/tasks/bump.js
@@ -1,28 +1,28 @@
 'use strict';
 
-import gulp from 'gulp';
-import help from 'gulp-help';
-import bump from 'gulp-bump';
-import conventionalRecommendedBump from 'conventional-recommended-bump';
-import path from 'path';
+var gulp = require('gulp');
+var help = require('gulp-help');
+var bump = require('gulp-bump');
+var conventionalRecommendedBump = require('conventional-recommended-bump');
+var path = require('path');
 
 // provide help through "gulp help" -- the help text is the second gulp task argument (https://www.npmjs.com/package/gulp-help/)
 help(gulp);
 
-gulp.task('bump', function (done) {
-  /**
-   * Bumping version number and tagging the repository with it.
-   * Please read http://semver.org/
-   *
-   * To bump the version numbers accordingly after you did a patch,
-   * introduced a feature or made a backwards-incompatible release.
-   */
+gulp.task('bump', function(done) {
+    /**
+     * Bumping version number and tagging the repository with it.
+     * Please read http://semver.org/
+     *
+     * To bump the version numbers accordingly after you did a patch,
+     * introduced a feature or made a backwards-incompatible release.
+     */
 
-  conventionalRecommendedBump({ preset: 'angular' }, function (err, result) {
-    // Get all the files to bump version in
-    gulp.src([path.resolve(__dirname, '../../', 'package.json')])
-      .pipe(bump({ type: result.releaseType }))
-      .pipe(gulp.dest(path.resolve(__dirname, '../../')));
-    done();
-  });
+    conventionalRecommendedBump({ preset: 'angular' }, function(err, result) {
+        // Get all the files to bump version in
+        gulp.src([path.resolve(__dirname, '../../', 'package.json')])
+            .pipe(bump({ type: result.releaseType }))
+            .pipe(gulp.dest(path.resolve(__dirname, '../../')));
+        done();
+    });
 });
--- gulp/tasks/changelog.js
+++ gulp/tasks/changelog.js
@@ -1,20 +1,16 @@
-import gulp from 'gulp';
-import help from 'gulp-help';
-import conventionalChangelog from 'gulp-conventional-changelog'
-import path from 'path';
+var gulp = require('gulp');
+var help = require('gulp-help');
+var conventionalChangelog = require('gulp-conventional-changelog');
+var path = require('path');
 
 // provide help through "gulp help" -- the help text is the second gulp task argument (https://www.npmjs.com/package/gulp-help/)
 help(gulp);
 
-gulp.task('changelog', function () {
+gulp.task('changelog', function() {
 
-  return gulp.src(path.resolve(__dirname, '../../', 'CHANGELOG.md'), {buffer: false})
-    .pipe(
-      conventionalChangelog(
-        {preset: 'angular', releaseCount: 0},
-        {},
-        {merges: undefined, noMerges: undefined}
-      )
-    )
-    .pipe(gulp.dest(path.resolve(__dirname, '../../')));
+    return gulp.src(path.resolve(__dirname, '../../', 'CHANGELOG.md'), { buffer: false })
+        .pipe(
+            conventionalChangelog({ preset: 'angular', releaseCount: 0 }, {}, { merges: undefined, noMerges: undefined })
+        )
+        .pipe(gulp.dest(path.resolve(__dirname, '../../')));
 });
