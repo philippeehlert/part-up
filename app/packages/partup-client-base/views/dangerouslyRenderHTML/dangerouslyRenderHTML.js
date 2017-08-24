970338d651d188755b0c925da89a9d759f9b0c9c
Léon Smit <lee_smit@live.nl>
2017-08-24 13:41:28 +0200

    fix(html): allow only needed tags

--- app/packages/partup-client-base/views/dangerouslyRenderHTML/dangerouslyRenderHTML.js
+++ app/packages/partup-client-base/views/dangerouslyRenderHTML/dangerouslyRenderHTML.js
@@ -1,13 +1,18 @@
 import sanitizeHTML from 'sanitize-html';
 
+const allowedHTMLTags = [
+    'p',
+    'a',
+    'img',
+    'span',
+];
+
 Template.dangerouslyRenderHTML.helpers({
     render: function() {
-        var template = Template.instance();
+        const template = Template.instance();
 
         return sanitizeHTML(template.data.HTML, {
-            allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-            'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-            'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'span'],
+            allowedTags: allowedHTMLTags,
             allowedAttributes: {
                 a: ['href', 'name', 'target'],
                 img: ['src'],
