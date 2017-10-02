206e669376d1a2bf7a6c7711339981225a7bea98
Noel Heesen <noelheesen@gmail.com>
2017-10-02 10:40:18 +0200

    fix(client): Add <em> and <strong> to MARKUP sanitize_access

--- app/packages/partup-client-base/views/dangerouslyRenderHTML/dangerouslyRenderHTML.js
+++ app/packages/partup-client-base/views/dangerouslyRenderHTML/dangerouslyRenderHTML.js
@@ -1,31 +1,30 @@
-// import sanitizeHTML from 'sanitize-html';
-
-// const allowedHTMLTags = [
-//     'p',
-//     'a',
-//     'img',
-//     'span',
-//     'svg',
-// ];
-
+/**
+ * Sanitize and set HTML dangerously
+ *
+ * this template is dependant on the following parameters to be given:
+ *
+ * @param {String} HTML The html string that needs to be sanitized
+ * @param {Function} transform [OPTIONAL] A function that modifies the html before it get's rendered
+ * @param {String} access [OPTIONAL=STANDARD] access used to determine which tags need sanitizing
+ * @example {{> dangerouslyRenderHTML
+ *  HTML=htmlString
+ *  transform=transformFunc
+ *  access="sanitize_access" }}
+ */
 Template.dangerouslyRenderHTML.onCreated(function() {
     const { transform = html => html } = this.data;
     this.transform = transform;
     this.access = this.data.access;
 });
 
-/**
- * Sanitize and set HTML dangerously
- *
- * {{> dangerouslyRenderHTML HTML=htmlString transform=transformFunction }}
- */
 Template.dangerouslyRenderHTML.helpers({
     render() {
         const template = Template.instance();
-        const transform = template.transform;
-        const transformed = transform(template.data.HTML);
-        const sanitizeAccess = template.access || 'standard';
+        const transformFunc = template.transform;
+        const transformedHTML = transformFunc(template.data.HTML);
+        const sanitizeAccess = template.access || Partup.client.html.SANITIZE_ACCESS.STANDARD;
+        const sanitizedHTML = Partup.client.html.sanitize(transformedHTML, sanitizeAccess);
 
-        return Partup.client.html.sanitize(transformed, sanitizeAccess);
+        return sanitizedHTML;
     },
 });
