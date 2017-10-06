86e97990caf14ec51aa201a9b48259f84b1536c5
René Post <renepost@annotatedjs.com>
2017-10-06 12:04:25 +0200

    fix(client): fix response on non-partner of a part-up trying to create new activity
    A user who tries to create a new activity in a part-up is asked if he wants to request to become a
    partner

--- app/packages/partup-client-pages/app/partup/activities/newactivity-restricted/newactivity-restricted.js
+++ app/packages/partup-client-pages/app/partup/activities/newactivity-restricted/newactivity-restricted.js
@@ -1,20 +1,42 @@
+function isPendingPartner(partupId) {
+
+        var user = Meteor.user();
+        if (!user) return false;
+        var partup = Partups.findOne(partupId);
+        if (!partup) return false;
+        return (partup.pending_partners || []).indexOf(user._id) > -1;
+}
+
 Template.app_partup_activities_newactivity_restricted.helpers({
+
     partup: function() {
         return Partups.findOne(this.partupId);
+    },
+
+    isPendingPartner: function() {
+        return isPendingPartner(this.partupId);
     }
+
 });
 
-Template.app_partup_activities_newactivity_restricted.events({
-    'click [data-newmessage]': function() {
-        Partup.client.popup.close();
 
-        // Use a defer to execute code after the route has changed
-        Meteor.defer(function() {
-            if (Router.current().route.getName() === 'partup') {
-                Partup.client.popup.open({
-                    id: 'new-message'
-                });
-            }
-        });
+Template.app_partup_activities_newactivity_restricted.events({ 
+
+    'click [data-open-takepart-popup]': function(event, template) {
+        if (Meteor.user()) {
+            Partup.client.popup.open({
+                id: 'take-part'
+            });
+        } else {
+            Intent.go({
+                route: 'login'
+            }, function(user) {
+                if (user) {
+                    Partup.client.popup.open({
+                        id: 'take-part'
+                    });
+                }
+            });
+        }
     }
 });
