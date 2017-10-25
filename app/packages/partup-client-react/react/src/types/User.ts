5313a0b42ff5be1500edbd494674ee1dd7311dfa
Rick Lancee <rickondraw@gmail.com>
2017-10-25 15:43:30 +0200

    feat(react): add user type

---
+++ app/packages/partup-client-react/react/src/types/User.ts
@@ -0,0 +1,69 @@
+export type Email = {
+    address: string,
+    verified: boolean
+}
+
+export interface Location {
+  city: string,
+  lat: number,
+  lng: number,
+  place_id: string,
+  country: string
+}
+
+export interface Profile {
+  name: string,
+  normalized_name: string,
+  settings: {
+    locale: string,
+    optionalDetailsCompleted: boolean,
+    email: {
+      dailydigest: boolean,
+      upper_mentioned_in_partup: boolean,
+      upper_mentioned_in_network_chat: boolean,
+      invite_upper_to_partup_activity: boolean,
+      invite_upper_to_network: boolean,
+      partup_created_in_network: boolean,
+      partups_networks_new_pending_upper: boolean,
+      partups_networks_accepted: boolean,
+      partups_new_comment_in_involved_conversation: boolean,
+      partups_networks_new_upper: boolean,
+      partups_networks_upper_left: boolean
+    },
+    unsubscribe_email_token: string
+  },
+  image: string|null,
+  description: string|null,
+  tags: Array<string>,
+  location: Location,
+  facebook_url: string|null,
+  twitter_url: string|null,
+  instagram_url: string|null,
+  linkedin_url: string|null,
+  phonenumber: string|null,
+  website: string|null,
+  skype: string|null,
+  meurs: {
+    portal: string,
+    nl_id: string,
+    en_id: string,
+    program_session_id: number,
+    fetched_results: boolean,
+    results: Array<{
+      code: number,
+      name: string,
+      score: number,
+      zscore: number,
+      dscore: number,
+      highIndex: number
+    }>
+  },
+  tiles: Array<string>
+}
+
+export interface User {
+  _id: string
+  emails: Array<Email>
+  profile: Profile,
+  _version: number
+}
