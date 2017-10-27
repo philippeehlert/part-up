fe39202352c97cc4ea57866536060663c0c97350
Léon Smit <lee_smit@live.nl>
2017-10-27 11:09:25 +0200

    feat(icons): better feedback when generating icons

--- app/packages/partup-client-react/react/generate-icons.js
+++ app/packages/partup-client-react/react/generate-icons.js
@@ -1,16 +1,24 @@
 'use strict';
 
-const testFolder = './src/static/icons';
+const iconsFolder = './src/static/icons';
+const outputFile = './src/static/icons.json';
 const fs = require('fs-extra');
 const SVGO = require('svgo');
 const svgo = new SVGO();
 const jsonObject = {};
 
-fs.readdirSync(testFolder).forEach(file => {
-    const filePath = `${testFolder}/${file}`;
+console.log(`Generating icons from ${iconsFolder}...`);
+
+// read icons in icons folder
+fs.readdirSync(iconsFolder).forEach(file => {
+    const filePath = `${iconsFolder}/${file}`;
+
+    // read content of icon svgs
     const contents = fs.readFileSync(filePath, 'utf8');
 
     svgo.optimize(contents, (optimizedContent) => {
+
+        if (!file.endsWith('.svg')) return console.log(`Skipped <${filePath}> because it isn't a .svg file.`);
 
         jsonObject[file.replace('.svg', '')] = optimizedContent.data
             .replace(/xmlns="(.*?)"/, '')
@@ -18,9 +26,13 @@ fs.readdirSync(testFolder).forEach(file => {
             .replace('<path', '<path fill="currentColor"')
             .replace(/width="(.*?)"/, '')
             .replace(/height="(.*?)"/, '');
+        
+        console.log(`Added <${file.replace('.svg', '')}> icon.`);
     });
 });
 
 const jsonString = JSON.stringify(jsonObject, null, 4);
 
-fs.writeFileSync('./src/static/icons.json', jsonString, {encoding: 'utf8'});
+fs.writeFileSync(outputFile, jsonString, {encoding: 'utf8'});
+
+console.log(`Finished creating icons <${outputFile}>.`);
