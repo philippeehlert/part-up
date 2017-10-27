4e62fc9d796604255791373f8cd542255055a078
Léon Smit <lee_smit@live.nl>
2017-10-27 11:00:02 +0200

    fix(icons): updated icons script to include optimize step

--- app/packages/partup-client-react/react/generate-icons.js
+++ app/packages/partup-client-react/react/generate-icons.js
@@ -1,17 +1,24 @@
+'use strict';
+
 const testFolder = './src/static/icons';
 const fs = require('fs-extra');
-
+const SVGO = require('svgo');
+const svgo = new SVGO();
 const jsonObject = {};
 
 fs.readdirSync(testFolder).forEach(file => {
-    const contents = fs.readFileSync(`${testFolder}/${file}`, 'utf8');
+    const filePath = `${testFolder}/${file}`;
+    const contents = fs.readFileSync(filePath, 'utf8');
 
-    jsonObject[file.replace('.svg', '')] = contents
-        .replace(/xmlns="(.*?)"/, '')
-        .replace(/fill="(.*?)"/, '')
-        .replace('<path', '<path fill="currentColor"')
-        .replace(/width="(.*?)"/, '')
-        .replace(/height="(.*?)"/, '');
+    svgo.optimize(contents, (optimizedContent) => {
+
+        jsonObject[file.replace('.svg', '')] = optimizedContent.data
+            .replace(/xmlns="(.*?)"/, '')
+            .replace(/fill="(.*?)"/, '')
+            .replace('<path', '<path fill="currentColor"')
+            .replace(/width="(.*?)"/, '')
+            .replace(/height="(.*?)"/, '');
+    });
 });
 
 const jsonString = JSON.stringify(jsonObject, null, 4);
