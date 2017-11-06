7adb64d184066e66436a66adf7097fbd146e83b5
Léon Smit <lee_smit@live.nl>
2017-11-06 20:36:05 +0100

    feat(fetcher): fetcher loading state

--- app/packages/partup-client-react/react/src/utils/Fetcher.ts
+++ app/packages/partup-client-react/react/src/utils/Fetcher.ts
@@ -9,25 +9,30 @@ type FetcherOptions<FetcherData = {[key: string]: any}> = {
     query?: QueryParameters;
     onChange?: Function;
     transformData?: (data: any) => FetcherData;
+    onResponse?: (data: any) => void;
 };
 
 export default class Fetcher<FetcherData = any> {
 
     public data: FetcherData;
+    public loading: boolean = false;
 
     private rawData: any;
     private route: string;
     private query: QueryParameters = {};
 
-    constructor({route, query, onChange, transformData}: FetcherOptions<FetcherData>) {
+    constructor({route, query, onChange, transformData, onResponse}: FetcherOptions<FetcherData>) {
         this.route = route;
         this.query = query || this.query;
         this.onChange = onChange || this.onChange;
+        this.onResponse = onResponse || this.onResponse;
         this.transformData = transformData || this.transformData;
         this.data = {} as FetcherData;
     }
 
     public async fetch() {
+        this.setLoading(true);
+
         const baseUrl = !process.env.REACT_APP_DEV ? '/' : 'http://localhost:3000/';
 
         try {
@@ -35,8 +40,9 @@ export default class Fetcher<FetcherData = any> {
             const responseJson = await response.json();
 
             this.rawData = responseJson;
+            this.onResponse(this.rawData); // call onresponse hook
             this.data = this.transformData(this.rawData);
-            this.onChange();
+            this.setLoading(false);
 
         } catch (err) {
             throw new Error(err);
@@ -47,6 +53,11 @@ export default class Fetcher<FetcherData = any> {
         this.onChange = () => {};
         this.transformData = (data: any) => data;
         this.data = {} as FetcherData;
+    }
+
+    private setLoading(value: boolean) {
+        this.loading = value;
+        this.onChange();
     }
 
     private getQueryParams(): string {
@@ -64,5 +75,6 @@ export default class Fetcher<FetcherData = any> {
 
     private onChange: Function = () => {};
     private transformData: (data: any) => FetcherData = (data: any) => data;
+    private onResponse: (data: any) => void = (data: any) => data;
 
 }
