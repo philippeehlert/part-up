0b207e0067e6b9c480c6c73622035cceefe146d7
Rick Lancee <rickondraw@gmail.com>
2017-11-29 13:21:34 +0100

    feat(react): add icon typings

--- app/packages/partup-client-react/react/src/components/Icon/Icon.tsx
+++ app/packages/partup-client-react/react/src/components/Icon/Icon.tsx
@@ -8,8 +8,65 @@ import * as icons from 'static/icons.json';
 interface Props {
     className?: string;
     onClick?: (event: React.MouseEvent<HTMLElement>) => void;
-    name: string;
+    name: IconName;
 }
+
+type IconName =
+      'archive'
+    | 'arrow-down'
+    | 'arrow-left'
+    | 'arrow-right'
+    | 'arrow-up'
+    | 'bell'
+    | 'calendar-check'
+    | 'caret-down'
+    | 'caret-left'
+    | 'caret-right'
+    | 'caret-slim-down'
+    | 'caret-slim-left'
+    | 'caret-slim-right'
+    | 'caret-slim-up'
+    | 'caret-up'
+    | 'chart'
+    | 'chat'
+    | 'check'
+    | 'checkbox'
+    | 'cog'
+    | 'crossroad'
+    | 'documents'
+    | 'download'
+    | 'drive'
+    | 'dropbox'
+    | 'facebook'
+    | 'globe'
+    | 'info-outline'
+    | 'info'
+    | 'instagram'
+    | 'linkedin'
+    | 'location'
+    | 'lock-closed'
+    | 'lock-open'
+    | 'megaphone'
+    | 'message'
+    | 'menu'
+    | 'monitor'
+    | 'monkeytail'
+    | 'network'
+    | 'pencil'
+    | 'person-plus'
+    | 'photo'
+    | 'plus'
+    | 'profile-card'
+    | 'question'
+    | 'search'
+    | 'share'
+    | 'show'
+    | 'times'
+    | 'twitter'
+    | 'unarchive'
+    | 'upload'
+    | 'video'
+    | 'warning';
 
 export class Icon extends React.Component<Props, {}> {
 
