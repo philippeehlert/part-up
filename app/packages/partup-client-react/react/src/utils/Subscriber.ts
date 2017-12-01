7e98b44bb5b68aae7194a2778e8e1d1e3c7f4df6
Léon Smit <lee_smit@live.nl>
2017-12-01 14:26:42 +0100

    fix(subscriber): subscriber typing

--- app/packages/partup-client-react/react/src/utils/Subscriber.ts
+++ app/packages/partup-client-react/react/src/utils/Subscriber.ts
@@ -2,8 +2,26 @@ import { Meteor } from 'utils/Meteor';
 import { defer } from 'lodash';
 
 interface SubscriberOptions {
-    subscription: string
-    onChange?: Function
+    subscription: string;
+    onChange?: Function;
+}
+
+interface Subscription {
+    subscriptionId: string;
+    ready: Function;
+    stop: Function;
+}
+
+interface DDPEvent {
+    id: string;
+    collection: string;
+    msg: string;
+    fields?: {
+        [param: string]: any;
+    };
+    cleared?: {
+        [param: string]: any;
+    };
 }
 
 export class Subscriber {
@@ -36,7 +54,7 @@ export class Subscriber {
                     this.onChange();
                     reject();
                 },
-            });
+            }) as Subscription;
 
             if (this.activeSubscription) this.activeSubscription.stop();
             this.activeSubscription = subscription;
@@ -57,13 +75,14 @@ export class Subscriber {
         };
     }
 
-    private onDataChange = (event: any) => {
-        defer(() => this.onChange());
+    private onDataChange = (event: DDPEvent) => {
+        defer(() => this.onChange(event));
     }
 
-    private track = (subscription: any) => {
+    private track = (subscription: Subscription) => {
         if (this.tracker) Meteor.ddp.off('changed', this.onDataChange);
         this.tracker = Meteor.ddp.on('changed', this.onDataChange);
+        console.log('tracker', this.tracker);
     }
 
     private onChange: Function = () => {
