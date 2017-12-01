efc8dee44c1058df56ecc9576c280f59f51904c8
Léon Smit <lee_smit@live.nl>
2017-12-01 14:24:59 +0100

    fix(images): images collection integration

--- app/packages/partup-client-react/react/src/collections/Images.ts
+++ app/packages/partup-client-react/react/src/collections/Images.ts
@@ -1,59 +1,26 @@
 import { get } from 'lodash';
-import { Meteor } from 'utils/Meteor';
+import { Collection, CollectionDocument } from 'collections/Collection';
 
-interface Image {
-    _id: string;
+interface Image extends CollectionDocument {
+    store: string;
 }
 
-const statics: Array<Image> = [];
+class ImagesCollection extends Collection<Image> {
 
-export function updateStatic(staticImages: Array<Image> = []) {
-    staticImages.forEach((image) => {
-        const index = statics.findIndex((element) => {
-            return element._id === image._id;
-        });
+    public getUrl = (image?: Image, store: string = '360x360'): string => {
+        const imageKey = get(image, `copies[${store}].key`);
 
-        if (index > -1) {
-            statics[index] = image;
-        } else {
-            statics.push(image);
-        }
-    });
+        if (!imageKey) return '';
+
+        const region = get(window, 'Meteor.settings.public.aws.region', 'eu-west-1');
+        const bucket = get(window, 'Meteor.settings.public.aws.bucket', 'partup-production');
+
+        // staging acceptance production aws image url
+        return `https://s3-${region}.amazonaws.com/${bucket}/${store}/${imageKey}`;
+    }
+
 }
 
-export function find(...args: any[]) {
-    return Meteor.collection('cfs.images.filerecord').find(...args) as Array<Image>;
-}
-
-export function findOne(...args: any[]) {
-    return Meteor.collection('cfs.images.filerecord').findOne(...args) as Image;
-}
-
-export function findStatic() {
-    return statics as Array<Image>;
-}
-
-export function findOneStatic(imageId: string) {
-    return statics.find((element) => element._id === imageId) as Image;
-}
-
-export function getUrl(image: Image, store: string = '360x360'): string {
-    const imageKey = get(image, `copies[${store}].key`);
-
-    if (!imageKey) return '';
-
-    const region = get(window, 'Meteor.settings.public.aws.region', 'eu-west-1');
-    const bucket = get(window, 'Meteor.settings.public.aws.bucket', 'partup-production');
-
-    // staging acceptance production aws image url
-    return `https://s3-${region}.amazonaws.com/${bucket}/${store}/${imageKey}`;
-}
-
-export const Images = {
-    updateStatic,
-    find,
-    findOne,
-    findStatic,
-    findOneStatic,
-    getUrl,
-};
+export const Images = new ImagesCollection({
+    collection: 'cfs.images.filerecord',
+});
