6a79f4773c1bd90e57fb2b9c75a7b8239cb701c9
Rick Lancee <rickondraw@gmail.com>
2017-12-07 11:59:48 +0100

    style(ActivityTile): add missing semicolon

--- app/packages/partup-client-react/react/src/components/ActivityTile/ActivityTile.tsx
+++ app/packages/partup-client-react/react/src/components/ActivityTile/ActivityTile.tsx
@@ -183,7 +183,7 @@ export class ActivityTile extends React.Component<Props, {}> {
                     </ModalPortal>
                 )}
             />
-        )
+        );
     }
 
     private archiveActivity = () => {
