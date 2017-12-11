d11d51f76410c6d41594f285e00f656f02c8f79d
Wouter (W.) Danes <wouter.danes@ing.nl>
2017-12-11 15:14:08 +0100

    fix(s3): Allow configuration of custom s3 host

--- app/packages/partup-server/s3.js
+++ app/packages/partup-server/s3.js
@@ -1,1 +1,10 @@
-S3 = new AWS.S3({ params: { Bucket: process.env.AWS_BUCKET_NAME } });
+options = {
+    params: { Bucket: process.env.AWS_BUCKET_NAME }
+};
+
+if (process.env.AWS_S3_ENDPOINT) {
+    options.endpoint = process.env.AWS_S3_ENDPOINT;
+    options.s3ForcePathStyle = true;
+}
+
+S3 = new AWS.S3(options);
