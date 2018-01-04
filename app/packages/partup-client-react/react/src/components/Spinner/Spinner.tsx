6d0b2b823a1bbff3fe32520c7dd8335fba76d909
Rick Lancee <rickondraw@gmail.com>
2018-01-04 12:15:57 +0100

    fix(Spinner): make spinner smaller

--- app/packages/partup-client-react/react/src/components/Spinner/Spinner.tsx
+++ app/packages/partup-client-react/react/src/components/Spinner/Spinner.tsx
@@ -14,22 +14,22 @@ export class Spinner extends React.Component<Props, {}> {
 
     public componentDidMount() {
         const options = {
-            lines: 10,  // The number of lines to draw
-            length: 8,  // The length of each line
-            width: 4,  // The line thickness
-            radius: 10,  // The radius of the inner circle
+            lines: 13,  // The number of lines to draw
+            length: 6,  // The length of each line
+            width: 2,  // The line thickness
+            radius: 6,  // The radius of the inner circle
             corners: 1,  // Corner roundness (0..1)
             rotate: 0,  // The rotation offset
             direction: 1,  // 1: clockwise, -1: counterclockwise
             color: '#000',  // #rgb or #rrggbb
-            speed: 1,  // Rounds per second
+            speed: 1.2,  // Rounds per second
             trail: 60,  // Afterglow percentage
             shadow: false,  // Whether to render a shadow
             hwaccel: false,  // Whether to use hardware acceleration
             className: '', // The CSS class to assign to the spinner
             zIndex: 0,  // The z-index (defaults to 2000000000)
-            top: '0',  // Top position relative to parent in px
-            left: '0',  // Left position relative to parent in px
+            top: '50%',  // Top position relative to parent in px
+            left: '50%'  // Left position relative to parent in px
         };
 
         new Spin(options).spin(this.spin);
