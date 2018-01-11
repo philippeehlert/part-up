e6643120fac8877bdfdf5cf5a469413294c0f821
Noel Heesen <noelheesen@gmail.com>
2018-01-11 16:39:28 +0100

    chore(packages): Remove sortablejs from dependencies

--- app/packages/partup-client-boardview/package.js
+++ app/packages/partup-client-boardview/package.js
@@ -2,24 +2,11 @@ Package.describe({
     name: 'partup-client-boardview',
     version: '0.0.1',
     summary: '',
-    documentation: null
+    documentation: null,
 });
 
 Package.onUse(function(api) {
-    api.use([
-        'templating',
-        'partup-lib',
-        'ecmascript',
-    ], 'client');
+    api.use(['templating', 'partup-lib', 'ecmascript'], 'client');
 
-    api.addFiles([
-        'Sortable.js',
-        'BoardView.html',
-        'BoardView.js'
-    ], 'client');
-});
-        // '.npm/package/node_modules/sortablejs/Sortable.min.js',
-
-Npm.depends({
-    "sortablejs": "1.5.1"//"1.5.0-rc1",
+    api.addFiles(['Sortable.js', 'BoardView.html', 'BoardView.js'], 'client');
 });
