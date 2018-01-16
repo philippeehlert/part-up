dd10cbb9b6a390be75fcb0301619ce86e82eb4ad
Noel Heesen <noelheesen@gmail.com>
2018-01-16 19:46:27 +0100

    feat(client): Store files in file-controller

--- app/packages/partup-client-files/pickers/onedrive-picker/onedrive-picker.js
+++ app/packages/partup-client-files/pickers/onedrive-picker/onedrive-picker.js
@@ -2,20 +2,61 @@ import _ from 'lodash';
 
 const onedrivePickerConfig = {
     clientId: 'a8ab6cd2-327e-4325-ba30-198c4d620278',
-    action: 'query',
+    action: 'share',
     multiSelect: true,
+    advanced: {
+        redirectUri: `${window.location.origin}/drivePicker`,
+        // filter: '' // for some reason the picker does not work when filters are applied
+    },
+};
+const onlyAllowedExtensions = _.partial(_.includes, Partup.helpers.files.extensions.all);
+
+const successCallback = (controller) => async (data) => {
+    const transformedFiles = _.map(
+        data.value,
+        Partup.helpers.files.transform.onedrive
+    );
+    const files = _.filter(controller.canAdd(transformedFiles, (removedFile) => {
+        const category = Partup.helpers.files.isImage(removedFile)
+            ? 'images'
+            : 'files';
+
+        Partup.client.nofity.info(
+            TAPi18n.__('upload-info-limit-reached', {
+                category,
+                filename: removedFile.name,
+            })
+        );
+    }), onlyAllowedExtensions);
+
+    controller.uploading.set(true);
+    const uploadPromises = [];
+    _.each(files, (file) => {
+        const uploadPromise = controller
+            .insertFileToCollection(file)
+            .then((inserted) => controller.addFilesToCache(inserted))
+            .catch((error) =>
+                Partup.client.notify.error(
+                    TAPi18n.__(`upload-error-${error.code}`)
+                )
+            );
+
+        uploadPromises.push(uploadPromise);
+    });
+
+    Promise.all(uploadPromises)
+        .catch((error) =>
+            Partup.client.notify.error(TAPi18n.__(`upload-error-100`))
+        )
+        .finally(() => controller.uploading.set(false));
 };
 
-const successCallback = async (files) => {
-    console.log(files);
-};
+const errorCallback = async (error) => Partup.client.notify.error(TAPi18n.__('upload-error-100'));
 
-const errorCallback = async (error) => console.log(error);
-
-const open = () => {
+const open = (controller) => () => {
     OneDrive.open({
         ...onedrivePickerConfig,
-        success: successCallback,
+        success: successCallback(controller),
         error: errorCallback,
         // cancel: '',
     });
@@ -36,7 +77,7 @@ Template.onedrivePicker.onRendered(function() {
         );
     }
 
-    this.$trigger.on('click', open);
+    this.$trigger.on('click', open(this.controller));
 });
 
 Template.onedrivePicker.onDestroyed(function() {
